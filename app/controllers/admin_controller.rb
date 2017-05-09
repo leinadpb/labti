039cf6e79d4ebe4301d567fe18b87fca4a2edbde
leinadpb <daniel.pena@intec.edu.do>
2017-05-09 09:04:35 -0400

    Upgrade v.5

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,38 +2,38 @@ class AdminController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
 
   def fd401
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd402
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd403
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd404
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd405
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd411
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd412
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd413
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd414
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
   def fd415
-    @subjects = Subject.all
+    @subjects = Subject.order(created_at: :desc)
   end
 
 

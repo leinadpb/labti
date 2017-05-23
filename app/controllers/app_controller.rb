2f13a9ddc9f7ceb3f97985c2325b4109faede106
leinadpb <daniel.pena@intec.edu.do>
2017-05-23 14:01:48 -0400

    Upgrade v.8

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -1,20 +1,7 @@
 class AppController < ApplicationController
 
-  class Materia
-    def initialize
-      @temp = Subject.order(:horai)
-    end
-    def get_subject(index)
-      @temp.find(index)
-    end
-    def get_title(index)
-      @temp.find(index).name
-    end
-  end
-
   def home
-    @tmp = Materia.new
-    @temp = Subject.order(:horai)
+    @subjects = Subject.order(:horai)
   end
 
   def fd401
